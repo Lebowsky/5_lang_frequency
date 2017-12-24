4042800902dfd195d7c5a293c261ecb87f49d392
Andrey Lebowski <andrey.lebowski@gmail.com>
2017-12-24 20:13:12 +0300

    first commit

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+The script displays ten most popular words in the text from the transferred file in descending order of frequency.
+
+# How to Use
+
+python3 lang_frequency.py <path to file>
+
+====Ten most popular words====
+the............ .............16
+be............. ..............6
+to............. ..............6
+a.............. ..............5
+is............. ..............5
+argument....... ..............4
+object......... ..............4
+of............. ..............4
+should......... ..............4
+calls.......... ..............3
+
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,28 @@
+import sys
+import string
+from collections import Counter
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, encoding='utf-8') as f:
+        return f.read()
 
 
-def get_most_frequent_words(text):
-    pass
+def get_most_frequent_words(text_array):
+    text_array = sorted(text_array.lower().translate(''.maketrans(
+        '', '', string.punctuation)).split())
+    counter = Counter()
+    for word in text_array:
+        counter[word] += 1
+    return counter
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) > 1:
+        text_data = load_data(sys.argv[1])
+        counter = get_most_frequent_words(text_data)
+        print('Ten most popular words'.center(30, '='))
+        for kw in counter.most_common()[0:10]:
+            print(kw[0].ljust(15, '.'), str(kw[1]).rjust(15, '.'))
+    else:
+        print('You must specify the path to the text file!')
