b82c62b9572e3e39b180635e2987acccad3ec160
Andrey Lebowski <andrey.lebowski@gmail.com>
2017-12-24 21:36:55 +0300

    correct variable names

--- lang_frequency.py
+++ lang_frequency.py
@@ -18,15 +18,15 @@ def get_most_frequent_words(text_array):
 
 
 if __name__ == '__main__':
-    LENGTH_STRING = 30
-    HALF_LENGTH_STRING = int(LENGTH_STRING/2)
-    NUMBER_OF_WORDS = 10
+    length_string = 30
+    half_length_string = int(length_string/2)
+    number_of_words = 10
 
     if len(sys.argv) > 1:
         text_data = load_data(sys.argv[1])
         counter = get_most_frequent_words(text_data)
-        print('Ten most popular words'.center(LENGTH_STRING, '='))
-        for kw in counter.most_common()[:NUMBER_OF_WORDS]:
-            print(kw[0].ljust(HALF_LENGTH_STRING, '.'), str(kw[1]).rjust(HALF_LENGTH_STRING, '.'))
+        print('Ten most popular words'.center(length_string, '='))
+        for kw in counter.most_common()[:number_of_words]:
+            print(kw[0].ljust(half_length_string, '.'), str(kw[1]).rjust(half_length_string, '.'))
     else:
         print('You must specify the path to the text file!')
