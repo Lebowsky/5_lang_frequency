8368e02594fd5316dcbb4b79a59d4ec055d8d44b
Andrey Lebowski <andrey.lebowski@gmail.com>
2017-12-24 21:07:48 +0300

    replace magical numbers

--- lang_frequency.py
+++ lang_frequency.py
@@ -18,11 +18,15 @@ def get_most_frequent_words(text_array):
 
 
 if __name__ == '__main__':
+    LENGTH_STRING = 30
+    HALF_LENGTH_STRING = int(LENGTH_STRING/2)
+    NUMBER_OF_WORDS = 10
+
     if len(sys.argv) > 1:
         text_data = load_data(sys.argv[1])
         counter = get_most_frequent_words(text_data)
-        print('Ten most popular words'.center(30, '='))
-        for kw in counter.most_common()[0:10]:
-            print(kw[0].ljust(15, '.'), str(kw[1]).rjust(15, '.'))
+        print('Ten most popular words'.center(LENGTH_STRING, '='))
+        for kw in counter.most_common()[:NUMBER_OF_WORDS]:
+            print(kw[0].ljust(HALF_LENGTH_STRING, '.'), str(kw[1]).rjust(HALF_LENGTH_STRING, '.'))
     else:
         print('You must specify the path to the text file!')
